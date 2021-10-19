d2d1698ee02c92053a6ceb72363421aeab595303
bruceshenqqe <bruceshenqqeq@gmail.com>
2021-10-19 10:30:53 -0400

    Update Constant.java

--- TeamProj/src/main/java/bfs/TeamProj/constant/Constant.java
+++ TeamProj/src/main/java/bfs/TeamProj/constant/Constant.java
@@ -5,9 +5,9 @@ public interface Constant {
     String SIGNING_KEY = "signingKey";
     String DEFAULT_AVATAR = "default avatar";
 
-    String ONBOARD_PENDING = "Onboard pending";
-    String ONBOARD_REJECTED = "Onboard rejected";
-    String ONBOARD_COMPLETED = "Onboard completed";
+    String ONBOARD_PENDING = "Onboarding pending";
+    String ONBOARD_REJECTED = "Onboarding rejected";
+    String ONBOARD_COMPLETED = "Onboarding completed";
     String OPT_PENDING = "OPT pending";
     String OPT_REJECTED = "OPT rejected";
     String OPT_COMPLETED = "OPT completed";
